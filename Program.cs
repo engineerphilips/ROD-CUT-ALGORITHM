340beafccec86e169c9d70277f595c24dc48abc5
phil <engineerphilips@outlook.com>
2023-04-05 04:47:12 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int BottomUpCutRod(int[] p, int n)
+{
+    var r = new int[n + 1];
+    r[0] = 0;
+
+    for (var j = 1; j <= n; j++)
+    {
+        var q = int.MinValue;
+        for (var i = 1; i <= j; i++)
+        {
+            q = Math.Max(q, p[i - 1] + r[j - i]);
+        }
+
+        r[j] = q;
+    }
+
+    return r[n];
+}
+
+var x = new[] {1, 5, 8, 9, 10};
+Console.WriteLine($"The optimum cost is : {BottomUpCutRod(x, 5)}");
